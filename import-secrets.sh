4cfbe4fa728ef36043cd64988b7a4850934bd4e9
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2025-05-20 07:12:29 -0400

    env examples

--- import-secrets.sh
+++ import-secrets.sh
@@ -9,7 +9,6 @@ fi
 SECRETS_DIR="$1"
 
 cp "$SECRETS_DIR/env.frontend" ./apps/frontend/.env # must be named .env or it will not be loaded
-cp "$SECRETS_DIR/env.backend" ./apps/backend/.env.backend # explicit name set in nestjs config service
-cp "$SECRETS_DIR/env.docker.backend" ./apps/backend/.env.docker.backend # explicit name set in docker-compose
+cp "$SECRETS_DIR/env.backend" ./apps/backend/.env # explicit name set in nestjs config service
 cp "$SECRETS_DIR/env.prisma" ./packages/prisma/.env # must be .env or it will not be loaded
 echo "Environment files copied into place."
