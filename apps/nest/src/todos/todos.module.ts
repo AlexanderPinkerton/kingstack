cc817b61ec9263fee01241c9cc6a2ecc8808bdcd
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2025-11-13 13:34:13 -0500

    fixed bug in AOS

--- apps/nest/src/todos/todos.module.ts
+++ apps/nest/src/todos/todos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { TodosController } from "./todos.controller";
+import { PublicTodosController } from "./public-todos.controller";
 import { TodosService } from "./todos.service";
 
 @Module({
-  controllers: [TodosController],
+  controllers: [TodosController, PublicTodosController],
   providers: [TodosService],
+  exports: [TodosService],
 })
 export class TodosModule {}
