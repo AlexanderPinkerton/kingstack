46e4d36813e10f4a57d1888984067b41cf8c55f5
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2025-11-26 10:57:13 -0500

    obey port

---
+++ apps/next/scripts/start-command.ts
@@ -0,0 +1,18 @@
+import { spawn } from "child_process";
+
+const PORT = process.env.PORT || "3069";
+
+console.log(`Starting Next.js Production Server on port ${PORT}...`);
+
+const next = spawn("next", ["start", "-p", PORT], {
+    stdio: "inherit",
+    shell: true,
+    env: {
+        ...process.env,
+        PORT,
+    },
+});
+
+next.on("exit", (code) => {
+    process.exit(code ?? 0);
+});
