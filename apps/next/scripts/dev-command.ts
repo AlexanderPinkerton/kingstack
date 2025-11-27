4bacee0fd4627ddb26700ee65f8ce4545d404cf7
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2025-11-26 19:25:06 -0500

    working local env now

--- apps/next/scripts/dev-command.ts
+++ apps/next/scripts/dev-command.ts
@@ -5,14 +5,14 @@ const PORT = process.env.PORT || "6666";
 console.log(`Starting Next.js Development Server on port ${PORT}...`);
 
 const next = spawn("next", ["dev", "-p", PORT, "--turbopack"], {
-    stdio: "inherit",
-    shell: true,
-    env: {
-        ...process.env,
-        PORT,
-    },
+  stdio: "inherit",
+  shell: true,
+  env: {
+    ...process.env,
+    PORT,
+  },
 });
 
 next.on("exit", (code) => {
-    process.exit(code ?? 0);
+  process.exit(code ?? 0);
 });
