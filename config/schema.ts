9dd3d6c34d8fa86ce01537234c1112a9ce6cd733
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2025-12-10 21:15:06 -0500

    switched to trusted publisher

--- config/schema.ts
+++ config/schema.ts
@@ -128,13 +128,6 @@ export const schema = defineSchema({
             default: "local",
             description: "Environment type: 'local' (http://localhost:PORT) or 'remote' (https://DOMAIN)",
         },
-
-        // CI/CD
-        NPM_TOKEN: {
-            required: false,
-            default: "xxx",
-            description: "NPM token for publishing packages",
-        },
     },
 
     // ============================================================================
@@ -309,7 +302,6 @@ export const schema = defineSchema({
                 "VERCEL_TOKEN",
                 "VERCEL_ORG_ID",
                 "VERCEL_PROJECT_ID",
-                "NPM_TOKEN",
             ],
         },
         vercel: {
